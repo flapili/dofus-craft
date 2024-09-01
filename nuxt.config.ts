ce530df97dfa5dbe8c8083d98c109ef3e2e11b2d
Benoit Deveaux <contact.flapili@gmail.com>
2024-09-01 11:41:35 +0200

    first deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
+
+  devtools: { enabled: true },
+
   ssr: false,
 
   modules: [
